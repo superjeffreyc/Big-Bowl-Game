f95f3100f4021b67ba594e93470cb2b1f88f3c94
Jeffrey Chan <jchan63@binghamton.edu>
2017-01-18 06:49:44 +0000

    Setting up templates after start screen

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -3,7 +3,20 @@
 $(document).ready(function() {
 
     $('#new_game_btn').click(function () {
-      $('#startScreen').hide();
+        $('#startScreen').hide();
+      
+        var lobby = document.querySelector('#lobby');
+        var content = document.importNode(lobby.content, true);
+        document.body.appendChild(content);
     });
+    
+    $('#add_words_btn').click(function () {
+        $('#startScreen').hide();
+      
+        var joinScreen = document.querySelector('#joinScreen');
+        var content = document.importNode(joinScreen.content, true);
+        document.body.appendChild(content);
+    });
+
 
 });
