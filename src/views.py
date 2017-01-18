0d88741167b601587816bf8dbb51fd247ef41b77
Jeffrey Chan <jchan63@binghamton.edu>
2017-01-18 21:48:55 +0000

    Add redirect to home page if room code is unknown

--- src/views.py
+++ src/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils.crypto import get_random_string
 from django.shortcuts import redirect
+from django.http import HttpResponsePermanentRedirect
 
 import random
 from .models import Room
@@ -13,11 +14,15 @@ def index(request):
 def lobby(request, roomCode=None):
 
     # If coming from the start screen or if the room code is invalid, create a new room code
-    if roomCode == "newgame" or len(Room.objects.all().filter(code = roomCode)) == 0:
+    if roomCode == "newgame":
         randomString = get_random_string(length=6, allowed_chars='abcdefghijkmnopqrstuvwxyz')   # Removed lowercase L since it may be interpreted as capital I
         room = Room(code = randomString)
         room.save()
         return redirect('lobby', roomCode = randomString)
+    
+    # Unknown room code - redirect to start screen
+    elif len(Room.objects.all().filter(code = roomCode)) == 0:
+        return HttpResponsePermanentRedirect("https://bigbowl.herokuapp.com/")
     
     return render(request, 'lobby.html', {'roomCode': roomCode})
     
