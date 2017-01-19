7dd204b75fe097cd78eca3ca86d88a1dba769f32
Jeffrey Chan <jchan63@binghamton.edu>
2017-01-19 05:51:16 +0000

    Added creation time to room model

--- src/models.py
+++ src/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Room(models.Model):
     code = models.CharField(max_length=6)
+    creation_time = models.DateTimeField('date created', auto_now_add=True, editable=False)
 
     class Meta:
         db_table = "room"
