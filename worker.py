60fc908180048168b905da2aa2a366138c4c71bc
Jeffrey Chan <jchan63@binghamton.edu>
2017-01-21 20:10:14 +0000

    Delete rooms more frequently now

--- worker.py
+++ worker.py
@@ -13,22 +13,26 @@ django.setup()
 from src.models import Room
 from src.models import WordBank
 
-# Heroku Scheduler runs this script everyday at midnight UTC.
-currentTime = timezone.now()
+# Heroku Scheduler runs this script every hour.
+eight_hours_ago = timezone.now()
+one_hour_ago = timezone.now()
 
 # Update timezone info for the currently timezone-unaware datetime object
-currentTime = currentTime.replace(tzinfo=pytz.UTC)
+eight_hours_ago = eight_hours_ago.replace(tzinfo=pytz.UTC)
+one_hour_ago = one_hour_ago.replace(tzinfo=pytz.UTC)
 
-# Subtract 24 hours to compare against current rooms
-currentTime -= datetime.timedelta(hours=24)
+# Subtract hours to compare against current rooms
+eight_hours_ago -= datetime.timedelta(hours=8)
+one_hour_ago -= datetime.timedelta(hours=1)
+
 
 # Get all rooms
 rooms = Room.objects.all()
 
-# Delete rooms that have existed for over 24 hours
+# Delete rooms that have existed for over 8 hours or rooms with no words that have existed for over 1 hour.
 for currentRoom in rooms:
 
-    if currentTime > currentRoom.creation_time:
+    if eight_hours_ago > currentRoom.creation_time or (one_hour_ago > currentRoom.creation_time and currentRoom.num_words == 0):
 
         words = WordBank.objects.all().filter(room = currentRoom)
 
