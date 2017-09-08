c8b5c41555def38df69831fbb83f1ca420f89ed7
Jeffrey Chan <superjeffreyc.cs@gmail.com>
2017-09-07 22:19:39 -0400

    Now delete all rooms after an hour

--- worker.py
+++ worker.py
@@ -1,4 +1,4 @@
-# This script removes rooms that have existed for over 24 hours
+# This script removes rooms that have existed for over an hour
 
 import django
 import os
@@ -14,25 +14,22 @@ from src.models import Room
 from src.models import WordBank
 
 # Heroku Scheduler runs this script every hour.
-eight_hours_ago = timezone.now()
 one_hour_ago = timezone.now()
 
 # Update timezone info for the currently timezone-unaware datetime object
-eight_hours_ago = eight_hours_ago.replace(tzinfo=pytz.UTC)
 one_hour_ago = one_hour_ago.replace(tzinfo=pytz.UTC)
 
 # Subtract hours to compare against current rooms
-eight_hours_ago -= datetime.timedelta(hours=8)
 one_hour_ago -= datetime.timedelta(hours=1)
 
 
 # Get all rooms
 rooms = Room.objects.all()
 
-# Delete rooms that have existed for over 8 hours or rooms with no words that have existed for over 1 hour.
+# Delete rooms that have existed for over 1 hour.
 for currentRoom in rooms:
 
-    if eight_hours_ago > currentRoom.creation_time or (one_hour_ago > currentRoom.creation_time and currentRoom.num_words == 0):
+    if one_hour_ago > currentRoom.creation_time:
 
         words = WordBank.objects.all().filter(room = currentRoom)
 
