ab0987030c9f687be5dc8345a54ca1880f9e3261
Jeffrey Chan <superjeffreyc.cs@gmail.com>
2022-09-20 13:24:57 -0400

    Add execute permission

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE = (
-    'sslify.middleware.SSLifyMiddleware',
+#    'sslify.middleware.SSLifyMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
