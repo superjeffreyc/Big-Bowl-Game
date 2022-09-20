39bcff80649825f987a7b2dcd0e841d794ca6672
Jeffrey Chan <10679173+superjeffreyc@users.noreply.github.com>
2022-09-20 13:43:31 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 export DATABASE_URL=postgres:///ubuntu
 export SECRET_KEY="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
-gunicorn config.wsgi --log-file -
+python3 manage.py runserver 0.0.0.0:8000
